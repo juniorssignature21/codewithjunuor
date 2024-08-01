a3bfa41f39f909cab6a8017577c27056cb4c3ff2
Junior's signature <chibuzor.john.2018@gmail.com>
2024-08-01 08:19:17 +0100

    added static_storage

--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 import os
 STATICFILES_DIRS = [
